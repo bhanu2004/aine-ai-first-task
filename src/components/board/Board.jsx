6c1a123f606c21b12ed62f8010ae52f4f1d2efd4
bhanu2004 <bhanuprksh05@gmail.com>
2023-12-27 21:25:13 +0530

    input updated

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './board.css'
 import { FaCaretLeft } from "react-icons/fa6";
 function Board() {
+  const files = useRef();
   return (
     <div className='board__container'>
         <div className="board__header">
@@ -10,7 +11,8 @@ function Board() {
         <div className="video__wrapper">
             <input className='input__url' type="text" placeholder='Insert URL here' />
             <p>Or</p>
-            <div className="upload__box">upload</div>
+            <input ref={files} className='fileInput' type="file" name="" id="" />
+            <div onClick={()=>{files.current.click()}} className="upload__box">upload</div>
         </div>
     </div>
   )
